21c52940bbe00dcd6b89ff9d846bf91d707c6d7b
Remi561 <138581483+Remi561@users.noreply.github.com>
2023-08-08 00:57:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function closeMenu(){
     centerRow.classList.remove("active")
     rightNavbar.classList.remove("active")
 }
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzjwpEZeEK2bQUtXz_8uLUTixttfhGFMnQvW19Yrgl9mcdFTsSMCD1thZSV9-Cjcyw/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
